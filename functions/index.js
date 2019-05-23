120472452d653ff57d4a2bbc653ff0f41a5c8aea
Drew Brantley <brantley@us.ibm.com>
2019-05-23 12:36:01 -0500

    add db read starter

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,17 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase');
 const express = require("express");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+var config = {
+    // apiKey: "apiKey",
+    authDomain: "express-firebase-1aa17.firebaseapp.com",
+    databaseURL: "https://express-firebase-1aa17.firebaseio.com",
+    // storageBucket: "bucket.appspot.com"
+};
+firebase.initializeApp(config);
+
+
+const fb_db = firebase.database();
 
 const app = express();
 
@@ -20,6 +28,26 @@ app.get("/timestamp",(req, res, next)=>{
     });
     last_date = date;
 });
+
+app.get("/runtime_vars", (req, res, next)=>{
+    res.json({
+        err:"You haven't done this yet silly! Also, this can't exist in new SW so I'll crash now.",
+    });
+    process.exit();
+});
+
+app.get("/database", async(req, res, next)=>{
+
+    await fb_db.ref("/").set({
+        "date":new Date().toString()
+    });
+
+    const snapshot = await fb_db.ref('/').once('value');
+
+    res.json({
+        snapshot
+    });
+})
 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //     const date = new Date();
