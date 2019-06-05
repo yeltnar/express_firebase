b8a378ff7626b10fa922ffe64bc6591034ac4a9f
Drew Brantley <brantley@us.ibm.com>
2019-06-05 18:28:42 -0500

    remove return that i didn't need

--- functions/interval_file.js
+++ functions/interval_file.js
@@ -6,22 +6,20 @@ const ONE_MIN = ONE_SEC*60;
 const ONE_HR = ONE_MIN*60;
 const ONE_DAY = ONE_HR*24;
 
-return
+// const interval_list = [
+//     {
+//         time:ONE_HR,
+//         f:async()=>{
+//             const person_id = "drew";
+//             const person = getPerson(person_id);
+//             console.log("setting new wallpaper");
+//             await requestP(`https://node.andbrant.com/phone/new_wallpaper?person_id=${person_id}&token=${person.key}`);
+//             console.log("set new wallpaper");
+//         }
+//     }
+// ];
 
-const interval_list = [
-    {
-        time:ONE_HR,
-        f:async()=>{
-            const person_id = "drew";
-            const person = getPerson(person_id);
-            console.log("setting new wallpaper");
-            await requestP(`https://node.andbrant.com/phone/new_wallpaper?person_id=${person_id}&token=${person.key}`);
-            console.log("set new wallpaper");
-        }
-    }
-];
-
-interval_list.forEach((cur)=>{
-    setInterval(cur.f, cur.time);
-});
+// interval_list.forEach((cur)=>{
+//     setInterval(cur.f, cur.time);
+// });
 
