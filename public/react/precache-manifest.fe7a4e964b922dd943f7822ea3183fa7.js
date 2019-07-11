26eb14f42019f322c9fb5b5ea2b167d175a484fb
Drew Brantley <brantley@us.ibm.com>
2019-07-11 10:17:34 -0500

    adding react in public folder

---
+++ public/react/precache-manifest.fe7a4e964b922dd943f7822ea3183fa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163940406b92315a5b312258a048f36f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5348fbb58db295d0e3c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "d5348fbb58db295d0e3c",
+    "url": "/static/js/main.e92d82b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
